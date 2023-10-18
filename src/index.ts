1507a53491829dc2b74ac4d4685f7136e8049d44
Admin <jhiguerasru@gmail.com>
2023-10-18 19:41:11 +0200

    build: :art: I do some thing
    array of the class

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,21 @@ import express, {type Express, type Request, type Response} from 'express';
 const app: Express = express();
 const port = process.env.HOST_PORT ?? '3000';
 
-const llistaClase = [];
+const listaClase = ['Garrido Pintado, Albert', 'Rodellar Martínez, Alberto', 'Muñoz Valenzuela, Alejandro', 'Pascual González, Alejandro', 'Blay Estrada, Alex', 'Bautista Martin, Brian', 'Llamas Masdeu, Dídac', 'Soto Gómez, Eugeni', 'Du Pre Peralta, Gerard', 'Higueras Rubiño, Jaime', 'Gàndara Sánchez, Jaume', 'Linares Vigo, Joan', 'Simón Pérez, Kevin', 'Sánchez Erazo, Luis Mario', 'Ros Puig, Manuel', 'Najarro García, Marc', 'Sans Lleixà, Martí', 'Argueta Navarrete, Nahomy Fernanda', 'Sanz Sanchez, Oscar', 'Condal García, Pol', 'Trujillo Albert, Pol', 'Pallarés Sirvent, Quim', 'Herrerias Cuscullola, Rafel', 'Trujillo Guadiamos, Victor Manuel'];
+const numTotalLista = listaClase.length;
 
+// eslint-disable-next-line arrow-body-style
+app.get('/', (req: Request, res: Response) => {
+	return res.send({listaClase});
+});
+// eslint-disable-next-line arrow-body-style
+app.get('/', (req: Request, res: Response) => {
+	return res.send({numTotalLista});
+});
 // eslint-disable-next-line arrow-body-style
 app.get('/', (req: Request, res: Response) => {
 	return res.send({message: 'Hello World!'});
 });
-
 app.listen(process.env.HOST_PORT, () => {
 	console.log(`⚡️[server]: Server is running at http://localhost:${port}!`);
 });
